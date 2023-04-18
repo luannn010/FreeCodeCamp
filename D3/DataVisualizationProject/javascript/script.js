24451d71ae33c2cd804cace5e281eb925772f045
luannn010 <103078945+luannn010@users.noreply.github.com>
2023-04-18 18:08:41 +1000

    Update
    Fix Barchart and Add ScatterPlot

--- D3/DataVisualizationProject/javascript/script.js
+++ D3/DataVisualizationProject/javascript/script.js
@@ -5,6 +5,8 @@ function BarChart() {
 
     d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
       .then((data) => {
+        var activeArea = null;
+        
         var data = data.data;
         
         var dataset = data.map((d) => {
@@ -26,13 +28,14 @@ function BarChart() {
                 quarter = "Q4"
 
         }
-            return { 
-              date: new Date(year, month-1, date[2]), 
-              gdp: d[1],
-              year: year,
-              quarter:quarter
-              
-            };
+            // Update date format in dataset creation
+        return { 
+          date: new Date(year, month-1, 1),  // always set day to 1
+          gdp: d[1],
+          year: year,
+          quarter: quarter
+        };
+
           });
 
         var w = 900;
@@ -46,11 +49,12 @@ function BarChart() {
           .attr("height", h + margin.top + margin.bottom)
           .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-        var xScale = d3.scaleTime()
+          
+          var xScale = d3.scaleTime()
           .domain([d3.min(dataset, (d) => d.date), d3.max(dataset, (d) => d.date)])
-          .range([0, w-100]);
-  
+          .range([0, w - 100]);
+          
+          
         var yScale = d3.scaleLinear()
           .domain([0, d3.max(dataset, (d) => d.gdp)])
           .range([h, 0]);
@@ -73,48 +77,54 @@ function BarChart() {
           .attr("class", "bar")
           .attr("x", (d) => xScale(d.date))
           .attr("y", (d) => yScale(d.gdp))
+          .attr("data-date", (d) => { return d3.timeFormat("%Y-%m-%d")(d.date); })
+          .attr("data-gdp", (d) => {  return d.gdp ;})
           .attr("width", w / dataset.length - barPadding)
           .attr("height", (d) => h - yScale(d.gdp))
           .attr("fill", "#03c6fc")
-          .on("mouseover", function( event, d) {
-            //change color
-            d3.select(this)
-               .style("fill", "orange");
-
-      
-            //Add tooltip
-
-var tooltip = d3.select("#tooltip");
-
-          tooltip.style("visibility", "visible")
-                 .html(d.year + " " + d.quarter + "<br>" + "$" + d.gdp + " Billion")
-                 .classed("visible", true) // add the "visible" class to the tooltip
-                 .style("left", (event.pageX - 175) + "px")
-                 .style("top", 450 + "px");
-
-      
- 
-              })
+          .on("mouseover", function(event, d) {
+            // Set active area
+            activeArea = d3.select(this);
           
-          .on("mouseout", function () {
-            d3.select(this)
-            .style("fill", "#03c6fc");
-          var tooltip = d3.select("#tooltip");
-          tooltip.style("visibility", "hidden")
-            .classed("visible", false); 
+            // Change color
+            activeArea.style("fill", "orange");
+            var date = activeArea.attr('data-date'); // e.g. 'Tue Jul 01 1986 00:00:00 GMT+1000 (AEDT)'
+            var formattedDate = d3.timeFormat('%Y-%m-%d')(new Date(date)); // e.g. '1986-07-01'
+            // Add tooltip
+            var tooltip = d3.select("#tooltip");
+          
+            tooltip.style("visibility", "visible")
+              .html(d.year + " " + d.quarter + "<br>" + "$" + d.gdp + " Billion")
+              .classed("visible", true) // add the "visible" class to the tooltip
+              .style("left", (event.pageX - 175) + "px")
+              .style("top", 450 + "px")
+              .attr('data-date', formattedDate); // set "data-date" attribute of tooltip
+          })
+                    
+          .on("mouseout", function() {
+            // Clear active area
+            activeArea = null;
+          
+            // Change color
+            d3.select(this).style("fill", "#03c6fc");
+          
+            var tooltip = d3.select("#tooltip");
+            tooltip.style("visibility", "hidden")
+              .classed("visible", false)
+              .attr("data-date", ""); // clear "data-date" attribute of tooltip
           });
-          
+                    
 
         
         //Draw xAxis
         svg.append("g")
-          .attr("id", "x_axis")
+          .attr("id", "x-axis")
           .attr("transform", "translate(0, " + (h - barPadding) + ")")
           .call(xAxis);
   
         //Draw yAxis
         svg.append("g")
-          .attr("id", "y_axis")
+          .attr("id", "y-axis")
           .attr("transform", "translate(" + (barPadding) + ",0)")
           .call(yAxis);
   
