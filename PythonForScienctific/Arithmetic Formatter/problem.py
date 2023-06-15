cdcb25ddb026014aafd098f128d861b080cc8d85
Luan Nguyen <luannn010@gmail.com>
2023-06-15 15:26:02 +0700

    p

--- PythonForScienctific/Arithmetic Formatter/problem.py
+++ PythonForScienctific/Arithmetic Formatter/problem.py
@@ -1,17 +1,19 @@
-from operate import Operator
 from dataclasses import dataclass, field
 
 @dataclass
-class Problem(Operator):
-    problemL : list
+class Problem:
+    problemL: list
     arguments: bool = field(default=False)
 
-    def has_less_then_4(self):
-        if  len(self.problemL) <= 4:
-            for item in self.problemL:
-                print(item)
+    def __init__(self, problemL, arguments=False):
+        if len(problemL) <= 4:
+            self.problemL = problemL
         else:
-            return "Error: Too many problems."
+            raise ValueError("Error: Too many problems.")
+        self.arguments = arguments
 
-    
+problem1 = Problem(["1", "2", "3", "4", "5"], True)  # Raises ValueError
+problem2 = Problem(["1", "2", "3", "4"], True)       # No error
 
+print(problem1)
+print(problem2)
